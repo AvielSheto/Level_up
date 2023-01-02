2829a9e3f8d12901d7d5b8c5757ac52a39c68fa8
AvielSheto <avielsheto@gmail.com>
2023-01-02 17:52:35 +0200

    Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='' element={<Home/>}/>
+      </Routes>
+    </Router>
   )
 }
