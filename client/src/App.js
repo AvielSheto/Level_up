3244a5147a2353bf69df6e1e828ffe6ba3efe0ce
AvielSheto <avielsheto@gmail.com>
2023-01-03 02:04:48 +0200

    create Home component and Room component that have deshbord and error component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Error from './pages/Error';
 import Home from './pages/Home';
+import Room from './pages/Room';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path='' element={<Home/>}/>
+        <Route path='room' element={<Room/>}/>
+        <Route path='*' element={<Error/>}/>
       </Routes>
     </Router>
   )
