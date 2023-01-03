59464e47abe7de3e0d62037086b0cc6ebc22a9c8
AvielSheto <avielsheto@gmail.com>
2023-01-03 02:52:57 +0200

    update navbar component

--- client/src/pages/PrivateArea.jsx
+++ client/src/pages/PrivateArea.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
 import UserDetails from '../Components/PrivateArea/UserDetails'
 
 export default function PrivateArea() {
   return (
     <>
-  <div>PrivateArea</div>
+  <Navbar />
   <UserDetails />
     </>
   )
