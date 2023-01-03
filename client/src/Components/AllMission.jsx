be50a2a63d42ae1d5ecb1cb86f5821287db395a9
MalakoN17 <bennynagato17@gmail.com>
2023-01-03 15:04:09 +0200

    update last update in allMissions component and change logo on home page

--- client/src/Components/AllMission.jsx
+++ client/src/Components/AllMission.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
-import MicrosoftLogo from '../assets/MicrosoftLogo-removebg-preview.png'
-import GoogleLogo from '../assets/GoogleLogo-removebg-preview.png'
-import biocatchLogo from '../assets/biocatchLogo-removebg-preview.png'
+import MicrosoftLogo from "../assets/MicrosoftLogo-removebg-preview.png";
+import GoogleLogo from "../assets/GoogleLogo-removebg-preview.png";
+import biocatchLogo from "../assets/biocatchLogo-removebg-preview.png";
 
 export default function AllMission() {
   return (
@@ -68,35 +68,37 @@ export default function AllMission() {
             <img src="" className="rounded-full" />
           </header>
           <content className="grid grid-cols-4 gap-3">
-            <div className="col-span-3 flex flex-col">
+            <div className="col-span-3 gap-2 flex flex-col">
               <subject className="font-bold text-lg pt-3">Google</subject>
+              <h3>project name: Google blog</h3>
               <description className="font-light text-sm pt-2">
-                This is one of the most sought-after hackathon ideas that give
-                you a chance to showcase your skills and abilities. To build a
-                creative blog, you would need to build a dynamic, smart website
-                which would require you to have top-notch skills in using HTML,
-                CSS, JavaScript, etc. You can also go the extra mile by using
-                some advanced tools like Angular, React.js, and Node.js. Your
-                blog could focus on solving the problem at hand, sharing
-                valuable knowledge in easily comprehensible bits and a lucid
-                language. It should also be search-friendly, well-indexed, and
-                user-friendly.
+                <span className="font-bold">description:</span> This is one of
+                the most sought-after hackathon ideas that give you a chance to
+                showcase your skills and abilities. To build a creative blog,
+                you would need to build a dynamic, smart website which would
+                require you to have top-notch skills in using HTML, CSS,
+                JavaScript, etc. You can also go the extra mile by using some
+                advanced tools like Angular, React.js, and Node.js. Your blog
+                could focus on solving the problem at hand, sharing valuable
+                knowledge in easily comprehensible bits and a lucid language. It
+                should also be search-friendly, well-indexed, and user-friendly.
               </description>
+              <div>
+              <Link
+                  to="/rooms"
+                  className="rounded bg-cyan-400 hover:bg-cyan-600 py-1 px-3 text-xs font-bold"
+                >
+                  Go live
+                </Link>
+              </div>
             </div>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center gap-5">
               <img src={GoogleLogo} />
               <div className="flex flex-col">
-              <span className="rounded bg-green-400 py-1 px-3 text-xs font-bold text-center hover:bg-green-600">
-                active
-              </span>
-              <Link
-                to="/rooms"
-                className="rounded bg-cyan-400 hover:bg-cyan-600 py-1 px-3 text-xs font-bold"
-              >
-                Go live
-              </Link>
+                <span className="rounded text-green-400 py-1 px-3  text-xs font-bold text-center">
+                  Active
+                </span>
               </div>
-
             </div>
           </content>
           <hr className="mt-5" />
@@ -108,22 +110,24 @@ export default function AllMission() {
           <content className="grid grid-cols-4 gap-3">
             <div className="col-span-3 flex flex-col">
               <subject className="font-bold text-lg pt-3">Microsoft</subject>
+              <h3>project name: Easy data</h3>
               <description className="font-light text-sm pt-2">
-                Data visualization is one of the most important and in-demand
-                skills in data science. Not everybody can make sense of complex
-                data but when it is presented to the audience in an
+                <span className="font-bold">description:</span> Data
+                visualization is one of the most important and in-demand skills
+                in data science. Not everybody can make sense of complex data
+                but when it is presented to the audience in an
                 easy-to-understand, visual format, it is a lot more
                 comprehensible. To deliver a successful hackathon project in
                 data visualization, you need to be well-versed in Python
                 programming and data science.
               </description>
             </div>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center gap-4">
               <img src={MicrosoftLogo} />
               <div className="flex flex-col">
-              <span className="rounded bg-red-400 py-1 px-3 text-xs font-bold text-center hover:bg-red-600">
-                Of line
-              </span>
+              <span className="rounded text-green-400 py-1 px-3  text-xs font-bold text-center">
+                  Active
+                </span>
               </div>
             </div>
           </content>
@@ -136,23 +140,25 @@ export default function AllMission() {
           <content className="grid grid-cols-4 gap-3">
             <div className="col-span-3 flex flex-col">
               <subject className="font-bold text-lg pt-3">BioCatch</subject>
+              <h3>project name: Gamification</h3>
               <description className="font-light text-sm pt-2">
-                Gamification is all the rage in the world right now. It is
-                helping people across industries to accomplish complex tasks and
-                keep audiences engaged. This top hackathon idea requires you to
-                be skilled in HTML5 and JavaScript. To deliver a gamification
-                project, you would need to first understand the problem
-                thoroughly and research thoroughly to gain some domain knowledge
-                before you can develop an interactive game to solve the
-                hackathon problem at hand.
+                <span className="font-bold">description:</span> Gamification is
+                all the rage in the world right now. It is helping people across
+                industries to accomplish complex tasks and keep audiences
+                engaged. This top hackathon idea requires you to be skilled in
+                HTML5 and JavaScript. To deliver a gamification project, you
+                would need to first understand the problem thoroughly and
+                research thoroughly to gain some domain knowledge before you can
+                develop an interactive game to solve the hackathon problem at
+                hand.
               </description>
             </div>
-            <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center gap-4">
               <img src={biocatchLogo} />
               <div className="flex flex-col">
-              <span className="rounded bg-red-400 py-1 px-3 text-xs font-bold text-center hover:bg-red-600">
-                Of line
-              </span>
+                <span className="rounded text-red-600 py-1 px-3 text-xs font-bold text-center ">
+                  Of line
+                </span>
               </div>
             </div>
           </content>
